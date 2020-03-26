0a1cb212c2b54dac63b54c2149d54ffa473d625c
Sasi Vinod Akula <sasivinod.akula@pamten.com>
2020-03-26 16:52:46 +0000

    Update Imagediff.java

--- src/main/java/com/uiFramework/pamTen/cpcommunity/helper/imagediffer/Imagediff.java
+++ src/main/java/com/uiFramework/pamTen/cpcommunity/helper/imagediffer/Imagediff.java
@@ -52,7 +52,7 @@ public class Imagediff {
 		System.out.println(actualImageFileName);
 		BufferedImage expectedImg = readFile(expectedImageFileName);
 		BufferedImage actualImg = readFile(actualImageFileName);
-		diff(expectedImg, expectedImg);
+		diff(expectedImg, actualImageFileName);
 	}
 
 }
