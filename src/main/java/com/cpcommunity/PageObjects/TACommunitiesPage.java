516eb65ba5997b18eb84b380eaf3b785fa7f5856
QA Team <andrew74444@gmail.com>
2020-09-23 22:07:44 +0530

    updated

--- src/main/java/com/cpcommunity/PageObjects/TACommunitiesPage.java
+++ src/main/java/com/cpcommunity/PageObjects/TACommunitiesPage.java
@@ -40,29 +40,35 @@ public class TACommunitiesPage extends BasePage{
 	@FindBy(xpath="//tbody/tr[1]/td[3]")
 	WebElement communityNameinFirstRow;
 
-//	@FindBy(xpath="")
-//	WebElement ;
+	@FindBy(xpath="((//tbody/tr[1]/td[2])/a[1])/span")
+	WebElement CommunityEdit;
 	
 	
 //		Community created successfully
+
+	
+	public void searchCommunity(String name) {
+		type(nameSearch, name, "Community Name Search");
+		click(searchBtn, "search Button");
+		WebElement ele = driver.findElement(By.xpath("//*[contains(text(),'"+name+"')]"));
+		waitForElementToPresent(ele);
+		SoftAssert st = new SoftAssert();
+		st.assertEquals(communityNameinFirstRow.getText(), name);
+	}
 	
 	
-	public void AddNewCommunity (String Name,String Networking,String Marketing,String BuildingRelationship,String Branding,String GrowMyBusiness,String InvestInBusiness,String Other,String About,String Category, String type) throws Exception {
+	
+	public void AddNewCommunity (String name,String Networking,String Marketing,String BuildingRelationship,String Branding,String GrowMyBusiness,String InvestInBusiness,String Other,String About,String Category, String type) throws Exception {
 			
 		click(createCommunity,"create Community");
 		waitForElementToPresent(pricingPlan);
 		scrollToElement(pricingPlan);
 		selectByVisibleText(pricingPlan, "All Plans 1", "pricing Plan");
 		scrollToElement(showing);
-		Name = Name+" "+date();
-		new CreateOrEditCommunityPage(driver).FillCommunityDetails(Name, Networking, Marketing, BuildingRelationship, Branding, GrowMyBusiness, InvestInBusiness, Other, About, Category, type);
+		name = name+" "+date();
+		new CreateOrEditCommunityPage(driver).FillCommunityDetails(name, Networking, Marketing, BuildingRelationship, Branding, GrowMyBusiness, InvestInBusiness, Other, About, Category, type);
+		searchCommunity(name);
 		
-		type(nameSearch, Name, "Community Name Search");
-		click(searchBtn, "search Button");
-		WebElement ele = driver.findElement(By.xpath("//*[contains(text(),'"+Name+"')]"));
-		waitForElementToPresent(ele);
-		SoftAssert st = new SoftAssert();
-		st.assertEquals(communityNameinFirstRow.getText(), Name);
 				
 		
 		
@@ -79,6 +85,20 @@ public class TACommunitiesPage extends BasePage{
 	protected ExpectedCondition getPageLoadCondition() {
 		
 		return ExpectedConditions.visibilityOf(createCommunity);
+	}
+
+
+
+
+
+
+
+	public void UpdateCommunity(String name,String Networking,String Marketing,String BuildingRelationship,String Branding,String GrowMyBusiness,String InvestInBusiness,String OtherName,String About,String Category,String type,String StateName,String CityName,String FacebookUrl,String TwitterUrl,String LinkedInUrl,String LogoImagePath,String websiteUrl,String ImagePath,String updateCommunityName) throws Exception {
+		name = name+" "+date();
+		searchCommunity(name);
+		updateCommunityName = updateCommunityName+" "+date();
+		click(CommunityEdit,"Community Edit");
+		new CreateOrEditCommunityPage(driver).UpdateCommunity(name, Networking, Marketing, BuildingRelationship, Branding, GrowMyBusiness, InvestInBusiness, OtherName, About, Category, type, StateName, CityName, FacebookUrl, TwitterUrl, LinkedInUrl, LogoImagePath, websiteUrl, ImagePath, updateCommunityName);
 	}
 
 	
---
+++ src/test/java/com/cpcommunity/testScripts/community/TC210_TA_Update_The_Community_Details.java
@@ -0,0 +1,49 @@
+package com.cpcommunity.testScripts.community;
+
+
+
+import java.util.Hashtable;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.cpcommunity.utilities.Constants;
+import com.cpcommunity.utilities.DataUtil;
+import com.cpcommunity.PageObjects.HomePage;
+import com.cpcommunity.PageObjects.LoginPage;
+import com.cpcommunity.PageObjects.PendingCommunitiesPage;
+import com.cpcommunity.PageObjects.SystemAdminDashboardPage;
+import com.cpcommunity.PageObjects.TACommunitiesPage;
+import com.cpcommunity.utilities.DataProviders;
+import com.cpcommunity.utilities.ExcelReader;
+
+public class TC210_TA_Update_The_Community_Details extends BaseTest {
+
+
+	
+	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
+	public void TC210(Hashtable<String,String> data) throws Exception {
+
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		DataUtil.checkExecution("master", "TC210", data.get("Runmode"), excel);
+		openBrowser(data.get("browser"));
+		logInfo("Launched Browser : "+data.get("browser"));
+		HomePage home = new HomePage().open();
+		LoginPage login = home.clickOnLOGINBtn();	
+		SystemAdminDashboardPage Dashboard_Page = login.SystemAdminloginToApplication(data.get("email"), data.get("password"));
+		TACommunitiesPage tACommunitiesPage =  Dashboard_Page.navigateToCommunitiesPage();
+		tACommunitiesPage.UpdateCommunity(data.get("communityName"), data.get("Networking"), data.get("Marketing"), data.get("BuildingRelationship"), data.get("Branding"), data.get("GrowMyBusiness"), data.get("InvestInBusiness"), data.get("OtherName"), data.get("About"), data.get("Category"), data.get("type"), data.get("StateName"), data.get("CityName"), data.get("FacebookUrl"), data.get("TwitterUrl"), data.get("LinkedInUrl"), data.get("LogoImagePath"), data.get("websiteUrl"), data.get("ImagePath"), data.get("updateCommunityName"));
+		
+//		Assert.fail("Failing the login test");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		
+		logInfo("TC205 Test Completed");
+		
+		quit();
+		
+	}
+
+}
