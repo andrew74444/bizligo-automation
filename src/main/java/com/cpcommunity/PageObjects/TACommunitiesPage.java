cf656afbd2f9c99456c80d657ba078f757760114
QA Team <andrew74444@gmail.com>
2020-09-23 20:18:28 +0530

    updated

--- src/main/java/com/cpcommunity/PageObjects/TACommunitiesPage.java
+++ src/main/java/com/cpcommunity/PageObjects/TACommunitiesPage.java
@@ -1,9 +1,11 @@
 package com.cpcommunity.PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.asserts.SoftAssert;
 import org.apache.log4j.Logger;
 
 
@@ -28,6 +30,20 @@ public class TACommunitiesPage extends BasePage{
 	@FindBy(xpath="//*[@class='toast-message']")
 	WebElement toastMessage;
 	
+	@FindBy(xpath="//input[@id='NameSearch']")
+	WebElement nameSearch;
+	
+	
+	@FindBy(xpath="//*[@class='fa fa-search']")
+	WebElement searchBtn;
+	
+	@FindBy(xpath="//tbody/tr[1]/td[3]")
+	WebElement communityNameinFirstRow;
+
+//	@FindBy(xpath="")
+//	WebElement ;
+	
+	
 //		Community created successfully
 	
 	
@@ -38,8 +54,18 @@ public class TACommunitiesPage extends BasePage{
 		scrollToElement(pricingPlan);
 		selectByVisibleText(pricingPlan, "All Plans 1", "pricing Plan");
 		scrollToElement(showing);
-		new CreateOrEditCommunityPage(driver).FillCommunityDetails(Name+" "+date(), Networking, Marketing, BuildingRelationship, Branding, GrowMyBusiness, InvestInBusiness, Other, About, Category, type);
-	
+		Name = Name+" "+date();
+		new CreateOrEditCommunityPage(driver).FillCommunityDetails(Name, Networking, Marketing, BuildingRelationship, Branding, GrowMyBusiness, InvestInBusiness, Other, About, Category, type);
+		
+		type(nameSearch, Name, "Community Name Search");
+		click(searchBtn, "search Button");
+		WebElement ele = driver.findElement(By.xpath("//*[contains(text(),'"+Name+"')]"));
+		waitForElementToPresent(ele);
+		SoftAssert st = new SoftAssert();
+		st.assertEquals(communityNameinFirstRow.getText(), Name);
+				
+		
+		
 		
 }
 
