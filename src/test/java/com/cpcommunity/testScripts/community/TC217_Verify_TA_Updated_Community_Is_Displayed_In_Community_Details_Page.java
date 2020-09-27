a6457eb66d95616546eed33406d2ea48776fd7ca
QA Team <andrew74444@gmail.com>
2020-09-27 22:02:26 +0530

    updated

---
+++ src/test/java/com/cpcommunity/testScripts/community/TC217_Verify_TA_Updated_Community_Is_Displayed_In_Community_Details_Page.java
@@ -0,0 +1,58 @@
+package com.cpcommunity.testScripts.community;
+
+
+
+import java.util.Hashtable;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.cpcommunity.utilities.Constants;
+import com.cpcommunity.utilities.DataUtil;
+import com.cpcommunity.PageObjects.CommunityDashboardPage;
+import com.cpcommunity.PageObjects.CommunityDetailsPage;
+import com.cpcommunity.PageObjects.EcoSystemPage;
+import com.cpcommunity.PageObjects.EditCommunityPage;
+import com.cpcommunity.PageObjects.GlobalCommunitesPage;
+import com.cpcommunity.PageObjects.HomePage;
+import com.cpcommunity.PageObjects.LoginPage;
+import com.cpcommunity.PageObjects.MyCommunitiesPage;
+import com.cpcommunity.PageObjects.MyDashboardPage;
+import com.cpcommunity.utilities.DataProviders;
+import com.cpcommunity.utilities.ExcelReader;
+
+public class TC217_Verify_TA_Updated_Community_Is_Displayed_In_Community_Details_Page extends BaseTest {	
+	
+	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
+	public void TC039(Hashtable<String,String> data) throws Exception {
+
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		DataUtil.checkExecution("master", "TC039", data.get("Runmode"), excel);
+		log.info("Inside Login Test");
+		openBrowser(data.get("browser"));
+		logInfo("Launched Browser : "+data.get("browser"));
+		HomePage home = new HomePage().open();
+		LoginPage login = home.clickOnLOGINBtn();
+//		login.login(data.get("username"), data.get("password"));
+//		logInfo("Username entered as "+data.get("username")+" and Password entered as "+data.get("password"));
+		EcoSystemPage EcoSystemPage = login.loginToApplication(data.get("communityAdminEmail"), data.get("password"));
+	
+//	
+		GlobalCommunitesPage GlobalCommunitesPage = EcoSystemPage.naviagtingToGlobalCommunities();
+		GlobalCommunitesPage.verifyCommunityCategory(data.get("communityName"));
+		CommunityDetailsPage CommunityDetailsPage = GlobalCommunitesPage.navigateToCommunityDetailsPage(data.get("communityName"));
+		CommunityDetailsPage.verifycommunityDetails(data.get("communityName"), data.get("About"), data.get("StateName"), data.get("CityName"), data.get("FacebookUrl"), data.get("LinkedInUrl"), data.get("TwitterUrl"), data.get("type"));
+//		CommunityDetailsPage.clickonLogo();
+		//Assert.fail("Failing the login test");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		
+		logInfo("Login Test Completed");
+		
+		quit();
+		
+	}
+
+}
