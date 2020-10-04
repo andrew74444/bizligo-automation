cb2e11bbc2dc49ca3d121d3591481ac2681bd1a4
andrew74444 <48715767+andrew74444@users.noreply.github.com>
2020-10-04 17:39:32 +0530

    updated

--- src/test/java/com/cpcommunity/testScripts/community/TC168_Verify_InActivated_Community_Is_Not_Displayed_In_Global_Communities.java
+++ src/test/java/com/cpcommunity/testScripts/community/TC168_Verify_InActivated_Community_Is_Not_Displayed_In_Global_Communities.java
@@ -19,21 +19,20 @@ public class TC168_Verify_InActivated_Community_Is_Not_Displayed_In_Global_Commu
 	
 	
 	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
-	public void TC041ToTC046(Hashtable<String,String> data) throws Exception {
+	public void TC168(Hashtable<String,String> data) throws Exception {
 		
 		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
-		DataUtil.checkExecution("master", "TC043", data.get("Runmode"), excel);
+		DataUtil.checkExecution("master", "TC168", data.get("Runmode"), excel);
 		log.info("Inside Login Test");
 		openBrowser(data.get("browser"));
 		logInfo("Launched Browser : "+data.get("browser"));
 		HomePage home = new HomePage().open();
-		LoginPage login = home.clickOnLOGINBtn();
-//		login.login(data.get("username"), data.get("password"));
-//		logInfo("Username entered as "+getHarley()+" and Password entered as "+getPassword());
-		EcoSystemPage EcoSystemPage = login.loginToApplication(data.get("email"), data.get("password"));
+		LoginPage loginPage = home.clickOnLOGINBtn();
+		EcoSystemPage EcoSystemPage = loginPage.loginToApplication(data.get("email"), data.get("password"));
+
 		
 		GlobalCommunitesPage GlobalCommunitesPage = EcoSystemPage.goToGlobalCommunities();
-		GlobalCommunitesPage.rejectCommunity(data.get("communityName"));
+		GlobalCommunitesPage.checkInActivatedCommunityIsNotDisplayed(data.get("communityName"));
 		
 		//Assert.fail("Failing the login test");
 	}
