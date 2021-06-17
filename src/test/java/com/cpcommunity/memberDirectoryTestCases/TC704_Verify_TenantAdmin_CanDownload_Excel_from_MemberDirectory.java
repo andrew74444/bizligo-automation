db81f1049ab4700ccff5754b954f53c9c5c434c2
ruchig18 <79819958+ruchig18@users.noreply.github.com>
2021-06-16 22:32:57 -0400

    Commit

---
+++ src/test/java/com/cpcommunity/memberDirectoryTestCases/TC704_Verify_TenantAdmin_CanDownload_Excel_from_MemberDirectory.java
@@ -0,0 +1,46 @@
+package com.cpcommunity.memberDirectoryTestCases;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.cpcommunity.PageObjects.HomePage;
+import com.cpcommunity.PageObjects.LoginPage;
+import com.cpcommunity.PageObjects.MemberDirectoryPage;
+import com.cpcommunity.PageObjects.MyCommunitiesPage;
+import com.cpcommunity.PageObjects.TA_DashboardPage;
+import com.cpcommunity.testScripts.community.BaseTest;
+import com.cpcommunity.utilities.Constants;
+import com.cpcommunity.utilities.DataProviders;
+import com.cpcommunity.utilities.DataUtil;
+import com.cpcommunity.utilities.ExcelReader;
+
+public class TC704_Verify_TenantAdmin_CanDownload_Excel_from_MemberDirectory extends BaseTest{
+	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
+	public void TC704(Hashtable<String,String> data) throws Exception {
+
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		DataUtil.checkExecution("master", "TC704", data.get("Runmode"), excel);
+		log.info("Inside Login Test");
+		openBrowser(data.get("browser"));
+		logInfo("Launched Browser : "+data.get("browser"));
+		logInfo("BizLigo Application Opened");
+		HomePage home = new HomePage().open(data.get("tenantType"));
+		LoginPage login = home.clickOnLOGINBtn();
+		TA_DashboardPage TDP = login.loginToApplicationTDP(data.get("email"), data.get("password"));
+		HomePage HP=TDP.goToHomePage();
+		MemberDirectoryPage MDP=HP.goToMemberDirectory();
+		MDP.downloadExcelFile(data.get("PathDownload"));
+	
+
+}
+	@AfterMethod
+	public void tearDown() {
+		
+		logInfo("TC704 Test Completed");
+		
+		quit();
+		
+	}
+}
