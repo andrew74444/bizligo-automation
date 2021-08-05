224f1dbb363dd2e7d4e6c4f1520de814213fd4c0
RoshaniUmredkar <roshani.umredkar@pamten.com>
2021-08-05 09:23:34 -0400

    Update TenantAdminDashboardPage.java

--- src/main/java/com/cpcommunity/PageObjects/TenantAdminDashboardPage.java
+++ src/main/java/com/cpcommunity/PageObjects/TenantAdminDashboardPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import junit.framework.Assert;
-
+//
 public class TenantAdminDashboardPage extends BasePage{
 
 	@Override
