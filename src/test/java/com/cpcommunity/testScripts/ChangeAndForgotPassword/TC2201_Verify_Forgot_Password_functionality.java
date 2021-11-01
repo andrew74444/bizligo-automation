9991fdfbedebf9054e24bcb320474aac9165c721
ruchig18 <79819958+ruchig18@users.noreply.github.com>
2021-10-31 21:45:46 -0400

    TA zoom Configeration

---
+++ src/test/java/com/cpcommunity/testScripts/ChangeAndForgotPassword/TC2201_Verify_Forgot_Password_functionality.java
@@ -0,0 +1,30 @@
+package com.cpcommunity.testScripts.ChangeAndForgotPassword;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.Test;
+
+import com.cpcommunity.PageObjects.HomePage;
+import com.cpcommunity.PageObjects.LoginPage;
+import com.cpcommunity.testScripts.community.BaseTest;
+import com.cpcommunity.utilities.Constants;
+import com.cpcommunity.utilities.DataProviders;
+import com.cpcommunity.utilities.DataUtil;
+import com.cpcommunity.utilities.ExcelReader;
+
+public class TC2201_Verify_Forgot_Password_functionality extends BaseTest{
+	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
+	public void TC2201(Hashtable<String,String> data ) throws Exception {
+
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		DataUtil.checkExecution("master", "TC2201", data.get("Runmode"), excel);
+		log.info("Inside Login Test");			
+		openBrowser(data.get("browser"));
+		logInfo("Launched Browser : "+data.get("browser"));	
+		logInfo("BizLigo Application Opened");
+		HomePage home = new HomePage().open(data.get("tenantType"));
+		LoginPage login = home.clickOnLOGINBtn();
+		login.forgotPassword(data.get("email"));
+
+}
+}
